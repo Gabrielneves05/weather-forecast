22fe1c609be848a52cd1a3dbe9e3a0ba7a2b5dae
Gabrielneves05 <gneves17@outlook.com>
2024-09-09 23:36:25 -0300

    chore: Axios data request / WeatherInfo component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useState, useRef } from 'react'
 import axios from 'axios'
 import './App.css'
+import WeatherInfo from './components/WeatherInfo/WeatherInfo';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [weather, setWeather] = useState({});
   const inputRef = useRef();
 
   async function searchCity() {
     const city = inputRef.current.value;
     const key = "575e56010fae56f60acaed7377bf0929";
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
 
     const weatherData = await axios.get(url);
-
-    console.log(weatherData);
+    setWeather(weatherData.data);
   }
 
   return (
@@ -22,6 +22,10 @@ function App() {
       <h1>Previsão do Tempo</h1>
       <input ref={inputRef} type="text" placeholder="Digite o nome da cidade"/>
       <button onClick={searchCity}>Buscar</button>
+
+      <WeatherInfo 
+        weather={weather}
+      />
     </div>
   )
 }
---
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -0,0 +1,11 @@
+
+
+function WeatherInfo({ weather }) {
+    return (
+        <div>
+            <h2>{weather.name}</h2>
+        </div>
+    )
+}
+
+export default WeatherInfo;
